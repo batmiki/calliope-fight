83acd4a432aaad27ef5b21eedc3ca800d6f84fa8
batmiki <45893394+batmiki@users.noreply.github.com>
2024-06-20 22:05:16 +0200

    Animatio

--- main.ts
+++ main.ts
@@ -1,25 +1,38 @@
-function init () {
-	
-}
-input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-	
-})
-init()
+let man = images.createImage(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+let ritter = images.createImage(`
+    # . # . .
+    . # # # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+let merged = images.createImage(`
+    . . # # .
+    . # # # #
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+man.showImage(4)
 basic.forever(function () {
     while (input.buttonIsPressed(Button.A)) {
+        man.scrollImage(-1, 500)
+        merged.showImage(0)
+        basic.setLedColor(0xff0000)
+        music.playMelody("D C D E E D C C ", 120)
+        ritter.scrollImage(1, 100)
         images.createImage(`
-            . . # . .
-            . # # # .
-            . . # . .
-            . # . # .
             . . . . .
-            `).scrollImage(-1, 500)
-        images.createImage(`
-            # . # . .
-            . # # # .
-            . . # . .
-            . # . # .
             . . . . .
-            `).scrollImage(1, 50)
+            . . . . .
+            . . . . .
+            . . . . .
+            `).showImage(0)
     }
 })
