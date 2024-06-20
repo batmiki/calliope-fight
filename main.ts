8ec2ce430014e5c832deae718d78a8bce03f7a8b
batmiki <45893394+batmiki@users.noreply.github.com>
2024-06-20 22:59:21 +0200

    Version 2

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
-    music.playMelody("C D E F G A B C5 ", 500)
+    music.playMelody("C D E F G A B C5 ", 600)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
     basic.setLedColor(0x00ff00)
-    man.scrollImage(-1, 500)
+    scrollRight(500000)
     merged.showImage(0)
     basic.setLedColor(0xff0000)
     music.playMelody("D C D E E D C C ", 120)
@@ -20,10 +20,42 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTT
         . . . . .
         `).showImage(0)
 })
+function scrollRight (num: number) {
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        `)
+    control.waitMicros(num)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # . . . .
+        . # . . .
+        . . . . .
+        `)
+    control.waitMicros(num)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . . .
+        # . # . .
+        . . . . .
+        `)
+    control.waitMicros(num)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+}
 let merged: Image = null
 let ritter: Image = null
-let man: Image = null
-man = images.createImage(`
+let man = images.createImage(`
     . . # . .
     . # # # .
     . . # . .
@@ -47,13 +79,4 @@ merged = images.createImage(`
 man.showImage(4)
 basic.forever(function () {
 	
-})
-control.inBackground(function () {
-    while (true) {
-        let bkg_musik = 0
-        control.waitMicros(100000)
-        if (bkg_musik != 0) {
-        	
-        }
-    }
 })
